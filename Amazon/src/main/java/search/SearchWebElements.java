00a13507f42801e991bf7cddad6dd3847227968d
ramdane1985 <ame.ramo1985@gmail.com>
2020-09-21 15:21:57 -0400

    adding

--- Amazon/src/main/java/search/SearchWebElements.java
+++ Amazon/src/main/java/search/SearchWebElements.java
@@ -2,8 +2,5 @@ package search;
 
 public class SearchWebElements {
 
-    public static final String searchFieldIDWebElement = "twotabsearchtextbox";
-    public static final String searchTextXpathWebElement = "//span[@class='a-color-state a-text-bold']\"";
-    public static final String searchHandSanitizerTitle = "Amazon.com : Hand Sanitizer";
 
 }
